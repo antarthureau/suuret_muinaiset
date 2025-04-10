c5ac5ffa4f6b06f6c58804bb43f54a51db3102f7
antoine.hureau <antoine.hureau@uib.no>
2025-04-10 23:45:21 +0200

    fixed audio playback
    pins 20, 21 and 23 cannot be used for relays and trigger line, so they are commented out and audio is back.

--- arduino/teensy_code_LONG/teensy_code_LONG.ino
+++ arduino/teensy_code_LONG/teensy_code_LONG.ino
@@ -4,9 +4,9 @@
   The circuit on a teensy 4.0:
 	- pin 6 as a digital output, using PWM (analogWrite function)
 	- pins 18 and 19 used as SCL and SPA for the RTC module
-  - auto ID player using pins 28 and 30
-	- pwm output goes through an IRL520 MOSFET, that itself drives a LED strip.
-  - relays are on 21, 20, 17 and 16
+  - auto ID player using pins 28 or 30
+	- PWM signal goes through an IRL520 MOSFET, that itself drives a LED strip.
+  - relays are on 21, 20, 17 and 16, PCB is marked incorrectly, so the cable must connect 1-4, 2-3, 3-2, 4-1
 
   created 21.08.2024
   by Antoine "Arthur" Hureau-Parreira
@@ -25,18 +25,18 @@
 
 //OBJECTS
 //audio
-AudioPlaySdWav sdWav;
-AudioAnalyzePeak audioPeak;
-AudioAnalyzeRMS audioRMS;
-AudioOutputI2S audioOutput;
-AudioControlSGTL5000 sgtl5000;
+AudioPlaySdWav           playWav1;
+AudioAnalyzePeak         audioPeak;
+AudioAnalyzeRMS          audioRMS;
+AudioOutputI2S           audioOutput;
+AudioControlSGTL5000     sgtl5000_1;
 //rtc
 RTC_DS3231 rtc;
 
 //AUDIO MATRIX
-AudioConnection patchCord1(sdWav, 0, audioOutput, 0);
-AudioConnection patchCord2(sdWav, 0, audioPeak, 0);
-AudioConnection patchCord3(sdWav, 0, audioRMS, 0);
+AudioConnection patchCord1(playWav1, 0, audioOutput, 0);
+AudioConnection patchCord2(playWav1, 0, audioPeak, 0);
+AudioConnection patchCord3(playWav1, 0, audioRMS, 0);
 
 //SD CARD
 #define SDCARD_CS_PIN 10
@@ -46,37 +46,40 @@ AudioConnection patchCord3(sdWav, 0, audioRMS, 0);
 //PINS
 const int REL_1 = 16;
 const int REL_2 = 17;
-const int REL_3 = 20;
-const int REL_4 = 21; //relay 4 is never used
+//const int REL_3 = 20;
+//const int REL_4 = 21; //relay 4 is never used
 const int LED_1 = 2;
 const int LED_2 = 3;
 const int LED_3 = 4;
 const int LED_4 = 5;
 const int PWM_PIN = 6;
 const int STATUS_PIN = 22;
-const int PLAYBACK_PIN = 23;
+//const int PLAYBACK_PIN = 23;
 const int SMALL_PIN = 30;
 const int SEASHELL_PIN = 28;
+const int LONG_PIN = 32;
 
 const uint8_t VOL_CTRL_PIN = A0;
 const uint8_t PWM_CTRL_PIN = A1;
 
 //VARIABLES
-float audioVolume = 0.2;
+float audioVolume = 0.5;
 int rangePWM = 255;
 bool systemStatus = false;
 bool playbackStatus = false;
-const bool PEAK_MODE = false;
+
+const bool PEAK_MODE = false; //Switch between peak or rms mode
 const char days[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 const int START_HOUR = 6;
 const int END_HOUR = 23;
-String SM_STR = "SMALL.WAV";
-String SS_STR = "SEASHELL.WAV";
-String LO_STR = "LONG.WAV";
-String FILE_NAME = "temp string";
-int PLAYER_ID = 4;
+const char SM_STR[13] = "SMALL.WAV";
+const char SS_STR[13] = "SEASHELL.WAV";
+const char LO_STR[13] = "LONG.WAV";
 
-const uint8_t REL_ARRAY[4] = {REL_1, REL_2, REL_3, REL_4};
+char FILE_NAME[13] = "";
+int PLAYER_ID;
+
+const uint8_t REL_ARRAY[4] = {REL_1, REL_2};
 const uint8_t LED_ARRAY[4] = {LED_1, LED_2, LED_3, LED_4};
 
 //SETUP
@@ -90,20 +93,20 @@ void setup() {
   }
   Serial.println("Leds array setup");
 
-  for (int i=0;i<4;i++){
+  for (int i=0;i<2;i++){
     pinMode(REL_ARRAY[i],OUTPUT);
   }
   Serial.println("Relays array setup");
 
   pinMode(PWM_PIN, OUTPUT);
   pinMode(STATUS_PIN, OUTPUT);
-  pinMode(PLAYBACK_PIN, OUTPUT);
+  //pinMode(PLAYBACK_PIN, OUTPUT);
   Serial.println("PWM and listen pins setup");
 
   //audio memory allocation, codec and volume setup
   AudioMemory(8);
-  sgtl5000.enable();
-  sgtl5000.volume(audioVolume);
+  sgtl5000_1.enable();
+  sgtl5000_1.volume(audioVolume);
 
   Serial.println("Audio memory allocated");
 
@@ -144,14 +147,21 @@ elapsedMillis fps;
 
 //LOOP
 void loop() {
-  statusUpdates();
+  //statusUpdates();
 
   if (PLAYER_ID == 0){
     digitalWrite(LED_1, HIGH);
   } else if (PLAYER_ID == 1){
-  digitalWrite(LED_2, HIGH);
-  }  else if (PLAYER_ID == 2){
-  digitalWrite(LED_3, HIGH);
+    digitalWrite(LED_2, HIGH);
+  } else if (PLAYER_ID == 2){
+    digitalWrite(LED_3, HIGH);
+  }
+
+  playWav1.play(FILE_NAME);
+  delay(25);
+
+  while (playWav1.isPlaying() == true){
+    writeOutPWM(PWM_PIN, PEAK_MODE);
   }
 }
 
@@ -227,7 +237,7 @@ void writeOutPWM(uint8_t pin, bool peak){
 void volumeControl(){
   float val = analogRead(VOL_CTRL_PIN);
   audioVolume = val / 1024; //10bits to 0-1 scale
-  sgtl5000.volume(audioVolume);
+  sgtl5000_1.volume(audioVolume);
 }
 
 //helper function to control the PWM range
@@ -261,14 +271,14 @@ void startupSequence(){
   delay(500);
   digitalWrite(REL_2, HIGH); //opens 36V DC line from the PSU to the amplifier
   delay(500);
-  digitalWrite(REL_3, HIGH); //opens audio line from the amplifier to the speaker
+  //digitalWrite(REL_3, HIGH); //opens audio line from the amplifier to the speaker
   systemStatus = true;
 }
 
 //helper function to sequence shutdown of the speaker, amp then psu
 void shutDownSequence(){
   digitalWrite(STATUS_PIN, HIGH);//send status signal to the other units
-  digitalWrite(REL_3, LOW); //closes audio line from the amplifier to the speaker
+  //digitalWrite(REL_3, LOW); //closes audio line from the amplifier to the speaker
   delay(500);
   digitalWrite(REL_2, LOW); //closes 36V DC line from the PSU to the amplifier
   delay(500);
@@ -278,7 +288,7 @@ void shutDownSequence(){
 
 //helper function to manage audio playback
 void playAudio(){
-  //sdWav.play(FILE_NAME);
+  playWav1.play(FILE_NAME);
   playbackStatus = true;
 
   Serial.print("Start playing ");
@@ -288,13 +298,14 @@ void playAudio(){
 //helper function to manage audio status and playback status booleans
 void statusUpdates(){
   //system status
-  if (PLAYER_ID == 0){  //if LONG player
+  if (PLAYER_ID == 0){ //if LONG player
     if (rtc.now().hour() >= START_HOUR && rtc.now().hour() < END_HOUR) {
       systemStatus = true;
     } else {
       systemStatus = false;
     }
-  } else {              //if SMALL/SEASHELL player
+  }
+  else { //if SMALL/SEASHELL player
     if (digitalRead(STATUS_PIN) == HIGH){
       systemStatus = true;
     } else {
@@ -303,7 +314,7 @@ void statusUpdates(){
   }
 
   //playback status
-  if (sdWav.isPlaying() == false) {
+  if (playWav1.isPlaying() == false) {
     playbackStatus = false;
   } else{
     playbackStatus = true;
@@ -314,16 +325,21 @@ void statusUpdates(){
 void setupPlayerID(){
   pinMode(SMALL_PIN,INPUT);
   pinMode(SEASHELL_PIN,INPUT);
+  pinMode(LONG_PIN, INPUT);
 
   if (digitalRead(SMALL_PIN) == HIGH){
     PLAYER_ID = 1;
-    FILE_NAME = SM_STR;
-  } else if (digitalRead(SEASHELL_PIN) == HIGH){
+    strcpy(FILE_NAME, SM_STR);
+  }
+  
+  if (digitalRead(SEASHELL_PIN) == HIGH){
     PLAYER_ID = 2;
-    FILE_NAME = SS_STR;
-  } else {
-  PLAYER_ID = 0;
-  FILE_NAME = LO_STR;
+    strcpy(FILE_NAME, SS_STR);
+  }
+  
+  if (digitalRead(LONG_PIN) == HIGH){
+    PLAYER_ID = 0;
+    strcpy(FILE_NAME, LO_STR);
   }
 
   Serial.print("Audio file setup ");
@@ -337,30 +353,20 @@ void leader(){
 
     if (playbackStatus == false){
       playAudio();
-      delay(10);
+      delay(25);
     }
 
     //write PWM during playback
-    while (sdWav.isPlaying() == true) {
+    while (playWav1.isPlaying() == true) {
       writeOutPWM(PWM_PIN, PEAK_MODE);
       //volumeControl();
       //pwmControl();
-    }
-
-    while (sdWav.isPlaying() == false) {
-      digitalWrite(PWM_PIN, LOW);
-      digitalWrite(PLAYBACK_PIN, LOW);
-      playbackStatus = false;
-      Serial.println("Playback finished");
-      delay(1000);
     }
   } 
 
   //IF SYSTEM IS ASLEEP
   else{
     shutDownSequence();
-    digitalWrite(PWM_PIN, LOW);
-    clockMe();
     Serial.print("I'm asleep and will wake up at ");
     Serial.println(START_HOUR);
   }
